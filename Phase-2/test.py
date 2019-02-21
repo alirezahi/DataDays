0cd837bba8074d26dab17e1d058f5f068c481c72
alirezaarv <alireza.arv.mail@gmail.com>
2019-02-21 16:26:48 +0330

    refactoring

---
+++ Phase-2/test.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+import numpy as np
+from joblib import dump, load
+from utils import load_data, CombinedAttr, SELECTED_FEATURES, DATA_PATH, TEST_DATA_PATH, TARGET_FIELDS
+
+pipe = load('pipe.joblib') 
+oe = load('oe.joblib') 
+SVG_model = load('svg.joblib') 
+
+test_posts = load_data(TEST_DATA_PATH, SELECTED_FEATURES)
+
+test_posts_prepared = pipe.transform(test_posts)
+predicted = SVG_model.predict(test_posts_prepared)
+
+
+for item in list(oe.inverse_transform([[i] for i in predicted]))[:20]: print(item)
