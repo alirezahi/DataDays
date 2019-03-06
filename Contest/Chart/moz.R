1beec1a8f0ba1d60398d61e394826762da950ef2
Amirreza Mozayani <amirrezamozayani@gmail.com>
2019-03-06 17:12:26 +0330

    mozzz

---
+++ Contest/Chart/moz.R
@@ -0,0 +1,53 @@
+# in the name of god
+library(dplyr)
+library(ggplot2)
+library(tidytext)
+library(RecordLinkage)
+library(tidyr)
+library(stringr)
+
+rm(list=ls())
+
+setwd("~/Documents/datadays/")
+td <- read.csv("Data/cafe_baazaar_dataset.csv", stringsAsFactors = F, nrow=3000)
+td$archive_by_user <- as.boolean(td$archive_by_user)
+td <- td %>% mutate(hour = unlist(strsplit(created_at, " "))[rep(c(F,T), nrow(td))])
+td <- td %>% mutate(day = unlist(strsplit(created_at, " "))[rep(c(T,F), nrow(td))])
+
+
+### single
+
+## time
+
+hour_dist <- data.frame(table(td$hour))
+hour_dist$Var1 <- factor(hour_dist$Var1, levels = c("12AM", "01AM", "02AM", "03AM", "04AM", "05AM", "06AM", "07AM","08AM","09AM","10AM","11AM","12PM","01PM", "02PM", "03PM", "04PM", "05PM", "06PM", "07PM","08PM","09PM","10PM","11PM"))
+ggplot(hour_dist, aes(Var1, Freq)) + geom_bar(stat = "identity")
+
+## archive percentage for each city
+
+td_archived <- filter(td, archive_by_user == T)
+
+td_city_archived <- td_archived %>%
+  group_by(city) %>% summarise(count = n())
+
+td_city <- td %>%
+  group_by(city) %>% summarise(count = n())
+
+td_city$count_archive <- td_city_archived$count
+td_city$per <- td_city$count_archive / td_city$count
+
+ggplot(td_city, aes(city, per, fill = city)) + geom_bar(stat="identity")
+
+
+
+### double
+
+
+plot_dis_day_city <- function(cat_name) {
+  td_specific_cat <- td %>% filter(cat1 == cat_name) %>% group_by(day) %>% summarise(count = n())
+  ggplot(td_specific_cat, aes(day, count, fill = day)) + geom_bar(stat = "identity")
+}
+plot_dis_day_city("personal")
+
+
+
